9828b8ca8be980b8a0efdbd350d3374694878069
Matt <matt@rcsipublishing.com>
2023-11-20 21:44:31 -0500

    Rollup migration and initialization

--- initialization/settings_init.sql
+++ initialization/settings_init.sql
@@ -7,7 +7,7 @@ INSERT INTO settings (setting, value, defaultvalue, modifiedon, type, help, disp
 --    ('use_ocr_for_transcript', '0', '0', NOW(), 'bool', 'Attempt to use OCR on comic page to get transcript.', 'Use OCR for Transcripts', null),
     ('generate_thumbnails', '1' ,'1', NOW(), 'bool', 'Auto generate thumbnails of uploaded pages.', 'Generate page thumbnails', null),
     ('allow_custom_themes', '1', '1', NOW(), 'bool', 'Allow users to generate custom themes for their comics.', 'Allow custom themes', null),
-    ('email_from_name', null, null, NOW(), 'string', 'Name used to send mail', 'From Name:', null),
-    ('email_from_address', null, null, NOW(), 'string', 'Email address used to send mail', 'From Address:', null),
-    ('admin_theme', null, null, NOW(), 'fileselect', 'Theme for ClickthuluFed Administration', 'Admin Theme:', 'public/css/adminthemes')
+    ('email_from_name', null, null, NOW(), 'string', 'Name used to send mail', 'From Name', null),
+    ('email_from_address', null, null, NOW(), 'string', 'Email address used to send mail', 'From Address', null),
+    ('admin_theme', null, null, NOW(), 'fileselect', 'Theme for ClickthuluFed Administration', 'Admin Theme', 'css/adminthemes/*.css')
 ;
